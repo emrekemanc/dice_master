9cd542e356c9b28a606b899b8f374b7a043d2ae0
Muhammet Emre Kemancı <k.emre.k25@gmail.com>
2025-03-04 13:37:17 +0300

    adding migration and starting auth

---
+++ src/user/dtos/user.dto.ts
@@ -0,0 +1,67 @@
+import { IsString, IsOptional, IsEmail, IsInt, IsUUID, IsEnum, IsDate } from 'class-validator';
+import { UserRole, UserStatus } from '@prisma/client';
+import { PartialType } from '@nestjs/mapped-types';
+
+export class UserDto {
+  @IsUUID()
+  @IsOptional()
+  id?: string;
+
+  @IsString()
+  firstName: string;
+
+  @IsString()
+  lastName: string;
+
+  @IsString()
+  userName: string;
+
+  @IsEmail()
+  mail: string;
+
+  @IsString()
+  @IsOptional()
+  msisdn?: string;
+
+  @IsInt()
+  @IsOptional()
+  userRank?: number;
+
+  @IsInt()
+  @IsOptional()
+  userXp?: number;
+
+  @IsEnum(UserRole)
+  @IsOptional()
+  userRole?: UserRole;
+
+  @IsEnum(UserStatus)
+  @IsOptional()
+  userStatus?: UserStatus;
+
+  @IsString()
+  @IsOptional()
+  avatarsId?: string;
+
+  @IsDate()
+  @IsOptional()
+  userBirthDate?: Date;
+
+  @IsDate()
+  @IsOptional()
+  createdAt?: Date;
+
+  @IsDate()
+  @IsOptional()
+  updatedAt?: Date;
+
+  @IsDate()
+  @IsOptional()
+  deletedAt?: Date;
+
+  @IsDate()
+  @IsOptional()
+  lastActive?: Date;
+}
+
+export class UpdateUserDto extends PartialType(UserDto) {}
